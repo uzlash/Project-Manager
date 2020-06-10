012d11e6021c26cc594b167f9db5ce3999ce26ab
Usman <Murtala>
2020-06-10 03:23:09 +0100

    initialized project manager vuetify app and added a toolbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <nav>
+            <v-toolbar flat app>
+                <v-toolbar-title class="grey--text text-uppercase">
+                    <span class="font-weight-light">Project</span>
+                    <span>Manager</span>
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn outlined color="grey">
+                    <span>Sign Out</span>
+                    <v-icon right>mdi-exit-to-app</v-icon>
+                </v-btn>
+            </v-toolbar>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+
+    })
+}
+</script>
